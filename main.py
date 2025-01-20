6e6630abf65fa371425589e4b5cd5978807bf1d5
M-6J <volatus_mj@naver.com>
2025-01-21 03:09:39 +0900

    asd

--- main.py
+++ main.py
@@ -214,9 +214,9 @@ if __name__ == '__main__':
         print('dvs_gesture')
         num_CLS = 11
         save_ds_name = 'dvs_gesture'
-        train_dataset = dvs128_gesture.DVS128Gesture(root='./dataset/DVS_Gesture', train=True, data_type='frame', frames_number=args.time_step,
+        train_dataset = dvs128_gesture.DVS128Gesture(root='/kaggle/input/test-dvs128/DVS  Gesture dataset', train=True, data_type='frame', frames_number=args.time_step,
                                                      split_by='number')
-        val_dataset = dvs128_gesture.DVS128Gesture(root='./dataset/DVS_Gesture', train=False, data_type='frame', frames_number=args.time_step,
+        val_dataset = dvs128_gesture.DVS128Gesture(root='/kaggle/input/test-dvs128/DVS  Gesture dataset', train=False, data_type='frame', frames_number=args.time_step,
                                                     split_by='number')
 
 
